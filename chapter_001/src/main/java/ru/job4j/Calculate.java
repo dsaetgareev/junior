4971b60a6319be264abbf1d0308fbfb0723f67d9
dsaetgareev <dinis0086@mail.ru>
2017-02-23 17:24:44 +0500

    add descreption Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+*Class Calculate, класс в котором находится единственный метод main.
+@author dsaetgareev
+@since 23.02.2017
+*/ 
 public class Calculate{
+	
+	/**
+	*метод main выводит на экран строку "Hello, World!"
+	*@param args - массив строку
+	*/
     public static void main(String[] args){
        System.out.println("Hello, world!");
     }
