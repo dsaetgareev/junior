4989d0d2d2eef5d33a60c1c027c0d10aefcfc6ec
dsaetgareev <dinis0086@mail.ru>
2017-02-23 17:15:38 +0500

    1.3.Java Doc

---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,10 @@
+/**
+*Class Calculate содержит метод main,
+*который выводит строку на экран
+*и содержит один параметр args типа массив строк.
+
+*@author Dinis Saetgareev (dinis0086@mail.ru)
+*@version 1.0
+*@since 23.02.2017
+*/
+package ru.job4j;
