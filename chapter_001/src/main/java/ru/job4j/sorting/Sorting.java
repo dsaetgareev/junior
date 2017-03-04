3f6efcb05cfd8cdc745483c2ef0f058cfe62096a
dsaetgareev <dinis0086@mail.ru>
2017-03-04 23:16:27 +0500

    5.1. chenged thread sort, add thread sortOptim

--- chapter_001/src/main/java/ru/job4j/sorting/Sorting.java
+++ chapter_001/src/main/java/ru/job4j/sorting/Sorting.java
@@ -3,8 +3,8 @@ package ru.job4j.sorting;
 /**
 *Class Sorting has thread int[] sort(int[] value).
 *@author Dinis Saetgareev (dinis0086@mail.ru)
-*@version 1.0
-*@since 03.03.2017
+*@version 2.0
+*@since 04.03.2017
 */
 
 public class Sorting {
@@ -27,6 +27,33 @@ public class Sorting {
 				 }
 			 }
 		 }
+		 return value;
+	 }
+
+	 /**
+	 *thread int[] sortOptim(int[] value)
+	 *sorting array by "Bubble".
+	 *@param value - int[]
+	 *@return value - array
+	 */
+	 public int[] sortOptim(int[] value) {
+		 int var;
+		for (int k = 0; k < value.length / 2; k++) {
+		 for (int i = 0; i != value.length - 1; i++) {
+			if (value[i] > value[i + 1]) {
+				 var = value[i];
+				 value[i] = value[i + 1];
+				 value[i + 1] = var;
+				 }
+		 }
+		 for (int j = value.length - 1; j > 0; j--) {
+			 if (value[j] < value[j - 1]) {
+				 var = value[j];
+				 value[j] = value[j - 1];
+				 value[j - 1] = var;
+			 }
+		 }
+		}
 		 return value;
 	 }
 }
