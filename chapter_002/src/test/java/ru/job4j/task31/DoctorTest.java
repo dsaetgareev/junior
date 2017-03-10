5dea59692e92b162a718b620187572a5ef89b58f
dsaetgareev <dinis0086@mail.ru>
2017-03-11 00:39:59 +0500

    3.1.Professional Class all file

---
+++ chapter_002/src/test/java/ru/job4j/task31/DoctorTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.task31;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * class DoctorTest checking methods class Doctor.
+ */
+public class DoctorTest {
+    /**
+     * method void whenHealThenHealth().
+     */
+    @Test
+    public void whenHealThenHealth() {
+        Professional prof = new Professional("Den", 30, "engineer", 10, 20, 30, 40);
+        Doctor doc = new Doctor("Alan", 29, "teacher", 30, 40, 50, 60);
+        doc.heal(prof);
+        assertThat(prof.getHealth(), is(30));
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/task31/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * class ProfessionalTest checking methods class Professional.
+ * class TeacherTest checking methods class Teacher.
+ * class DoctorTest checking methods class Doctor.
+ * class EngineerTest checking methods class Engineer.
+ */
+package ru.job4j.task31;
