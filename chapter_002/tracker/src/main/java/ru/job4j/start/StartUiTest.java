9126fc1676f63cf611932389b0bd441d2a25f87f
dsaetgareev <dinis0086@mail.ru>
2017-03-19 16:24:06 +0500

    not working StartUITest and StartUiTest

--- chapter_002/tracker/src/main/java/ru/job4j/start/StartUiTest.java
+++ chapter_002/tracker/src/main/java/ru/job4j/start/StartUiTest.java
@@ -1,18 +1,23 @@
 package ru.job4j.start;
 
 /**
- * class StartUITest - testing StartUI class.
+ * class StartUiTest - testing StartUI class.
  * @author Dinis Saetgareev (dinis0086@mail.ru)
  * @since 15.03.2017
  * @version 1.0
  */
-public class StartUITest {
+public class StartUiTest {
     /**
      * method main.
      * @param args - String[]
      */
     public static void main(String[] args) {
-        Input input = new StubInput(new String[] {"0", " test1", "checing test1", "15032017", "method add", " ", "1", "y"});
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[] {"1", "0", " test1", "checking test1", "15032017", "method add",
+                "a", "0", "test2", "checking test2", "170317", "method add", "a", "1", " ", "2", tracker.findAll()[0].getId(),
+                "test3", "test3", "170317", "test3", " ", "3", tracker.findAll()[0].getId(), " ", "6", " ", "7",
+                tracker.getDeleteItems()[0].getId(), " ", "4", tracker.findAll()[1].getId(), " ", "test3", " ",
+                "8", tracker.findAll()[1].getId(), "new ", " ", "1", "exit"});
         new StartUI(input).init();
     }
 }
--- chapter_002/tracker/src/test/java/ru/job4j/start/StartUITest.java
+++ chapter_002/tracker/src/test/java/ru/job4j/start/StartUITest.java
@@ -1,7 +1,25 @@
 package ru.job4j.start;
 
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * Created by dinis on 18.03.2017.
+ * class StartUiTest - testing StartUI class.
+ * @author Dinis Saetgareev (dinis0086@mail.ru)
+ * @since 15.03.2017
+ * @version 1.0
  */
 public class StartUITest {
+    /**
+     * void whenAddItemThenResult().
+     */
+    @Test
+    public void whenAddItemThenResult() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[] {"1", "0", "test1", "test2", "123", "test1", "exit"});
+        new StartUI(input).init();
+        Item item = new Item(tracker.findAll()[0].getId(), "test1", "test2", 123, "test1");
+        assertThat(tracker.findAll()[0], is(item));
+    }
 }
